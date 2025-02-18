602bfc8acedaf2dab86c0af41e89b9ecff783ca7
Ian Seki <ian.seki@ufu.br>
2025-02-18 15:05:21 -0300

    Primeira Ação

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Modific"}
+    return {"Hello": "lalalalalalalalalalala"}
 
 @app.get("/quadrado/{num}")
 def square(num: int):
