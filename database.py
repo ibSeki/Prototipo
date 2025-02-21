80bd7afb8eb671bb785b37d6832ef2ffeccbf530
Ian Seki <ian.seki@ufu.br>
2025-02-21 09:48:42 -0300

    Vinculação React Front End

--- database.py
+++ database.py
@@ -1,18 +1,27 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-user = "postgres"
-password = "Batleforce*3"
-database = "nome_database"
-host = "localhost"
+# Carregar as variáveis do arquivo .env
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
+# Pegando as credenciais do banco de dados
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "5432")  # Porta padrão do PostgreSQL
+
+# Configuração correta da URL de conexão
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker( autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+# Função para obter a sessão do banco
 def get_db():
     db = SessionLocal()
     try:
